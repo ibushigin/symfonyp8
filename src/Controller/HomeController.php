a0cf7774265f9a96f30181a7c7208f461793b5ec
ibushigin <rq6v6k3>
2018-12-20 12:30:53 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,4 +14,15 @@ class HomeController{
  public function bonjour(){
    return new Response('<html><body><strong>Bonjour</strong></body></html>');
  }
+
+ //créer une page pour l'url /exercice1/comment-allez-vous qui affiche "bien, merci"
+
+ /**
+ *Grace aux annotations, je peux peux déclarer ma route
+ *@Route ("/exercice1/comment-allez-vous")
+ *
+ */
+ public function cav(){
+   return new Response('<html><body><strong>Bien, merci!</strong></body></html>');
+ }
 }
