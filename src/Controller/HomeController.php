c412527fc7745b5d61d5964cef6f0bb72d04496a
ibushigin <rq6v6k3>
2018-12-20 15:56:45 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,7 +19,8 @@ class HomeController extends AbstractController{
  }
 
  /**
- * @Route ("/bonjour/{nom}")
+  * On peut control ece que va contenir le placeholder avec une regex
+ * @Route ("/bonjour/{nom}", name="bonjourNom", requirements={"nom"="[a-z]+"})
  */
  public function bonjourPseudo($nom){
      return $this->render('bonjourPseudo.html.twig', array('pseudo'=> $nom));
@@ -28,7 +29,7 @@ class HomeController extends AbstractController{
 
  /**
  *Grace aux annotations, je peux peux déclarer ma route
- *@Route ("/")
+ *@Route ("/", name="Home")
  *
  */
  public function home(){
@@ -43,5 +44,16 @@ public function heure(){
     $date = date("Y-m-d");
     $hour = date("H:i:s");
     return $this->render('exercice.html.twig', array('date'=>$date, 'hour'=>$hour));
+}
+
+
+/**
+ * méthode qui va faire une redirect vers la page d'accueil
+ * @Route("/redirect")
+ */
+
+public function redirectHome(){
+    //Pour faire une redirect en param le nom de la route vers laquelle on veut rediriger
+    return $this->redirectToRoute('home');
 }
 }
